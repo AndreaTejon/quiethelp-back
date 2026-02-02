cd7f46b5d0446d112a42da5340d593408cb6a557
andrea tejon vega <andreatejon21@gmail.com>
2026-02-02 09:28:18 +0100

    commit inicial proyecto vacio

---
+++ quiethelp/src/main/java/com/trinitarias/quiethelp/QuiethelpApplication.java
@@ -0,0 +1,13 @@
+package com.trinitarias.quiethelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiethelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiethelpApplication.class, args);
+	}
+
+}
---
+++ quiethelp/src/test/java/com/trinitarias/quiethelp/QuiethelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinitarias.quiethelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiethelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
